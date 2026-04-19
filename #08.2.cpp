c694a4131b8ff276b461f52c51d2709f374e312b
OSAMA <zxod052@gmail.com>
2026-04-20 02:35:06 +0400

    Add first batch of Level 2 algorithm solutions
    Solving Level 2 problems including digit manipulation, palindrome checks, and nested loop patterns.

---
+++ #08.2.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadPositiveNumber(string Message)
+{
+	int Number = 0;
+
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+int CountDigitFrequency(int DigitToCheck , int Number)
+{
+	int FreqCount = 0;
+	int Remainder = 0;
+
+	while (Number > 0)
+	{
+		Remainder = Number % 10;
+		Number = Number / 10;
+		if (DigitToCheck == Remainder)
+		{
+			FreqCount++;
+		}
+	}
+	return FreqCount;
+}
+int main()
+{
+	int Number = ReadPositiveNumber("Please enter the main number?");
+	int DigitToCheck = ReadPositiveNumber("Please enter one digit to check?");
+
+	cout << "\nDigit " << DigitToCheck << " Frequency is "
+		   << CountDigitFrequency(Number, DigitToCheck) << " Time(s).";
+		                              
+
+	return 0;
+}
